9ab9ac28b6e2f1c2d22914b26e7cc02ae1260a06
Chetan <cashtivkar@technosofteng.com>
2019-09-10 18:59:56 +0530

    Finished project setup Added necessary gradle dependencies. Added common classes to keep common code at one place.

---
+++ app/src/main/java/com/billy/meow/view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.billy.meow.view
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.billy.meow.R
+import com.billy.meow.common.BaseActivity
+import com.billy.meow.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main, component)
+    }
+}
---
+++ app/src/main/java/com/billy/meow/viewmodel/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.billy.meow.viewmodel
+
+import android.app.Application
+import com.billy.meow.common.BaseViewModel
+
+/**
+ * Created by Chetan on 10-09-2019.
+ */
+class MainViewModel(application: Application) : BaseViewModel(application) {
+
+    init {
+
+    }
+}
